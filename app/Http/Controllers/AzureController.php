25374626243818111e9d232525f22a5530d7e699
ALSINA\KCubias <kcubias@alsina.com>
2025-01-23 13:06:06 +0100

    Feat - Los usuarios que tienen foto de perfil en azure se muestra

--- app/Http/Controllers/AzureController.php
+++ app/Http/Controllers/AzureController.php
@@ -10,6 +10,8 @@ use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+
 
 class AzureController extends Controller
 {
@@ -17,36 +19,68 @@ class AzureController extends Controller
     {
         return Socialite::driver('azure')->redirect();
     }
-
     public function handleAzureCallback()
     {
         try {
             $azureUser = Socialite::driver('azure')->user();
             $token = $azureUser->token;
 
-            // Crear una instancia del cliente HTTP
             $client = new Client();
+
             $response = $client->get('https://graph.microsoft.com/v1.0/me?$select=mail,userPrincipalName,displayName,officeLocation,companyName,jobTitle,department,city,country', [
                 'headers' => [
                     'Authorization' => 'Bearer ' . $token,
                     'Accept' => 'application/json',
                 ],
             ]);
+
             if ($response->getStatusCode() !== 200) {
                 throw new \Exception('Error al obtener los datos del perfil de usuario');
             }
-            // Convertir la respuesta JSON a un array PHP
+
             $userProfile = json_decode($response->getBody()->getContents(), true);
+
+            // Intentar obtener la foto de perfil del usuario
+            $photoUrl = 'https://graph.microsoft.com/v1.0/me/photo/$value';
+
+            try {
+                $photoResponse = $client->get($photoUrl, [
+                    'headers' => [
+                        'Authorization' => 'Bearer ' . $token,
+                    ],
+                ]);
+
+                if ($photoResponse->getStatusCode() === 200) {
+                    $contentType = $photoResponse->getHeaderLine('Content-Type');
+
+                    if (in_array($contentType, ['image/jpeg', 'image/png', 'image/gif'])) {
+
+                        $photoContent = $photoResponse->getBody()->getContents();
+                        $photoPath = 'users-avatar/' . $userProfile['userPrincipalName'] . '.jpg';
+                        Storage::disk('public')->put($photoPath, $photoContent);
+                        $userProfile['photo_path'] = $photoPath;
+                    } else {
+
+                        $userProfile['photo_path'] = null;
+                    }
+                } else {
+
+                    $userProfile['photo_path'] = null;
+                }
+            } catch (\Exception $e) {
+
+                $userProfile['photo_path'] = null;
+            }
 
             $user = $this->findOrcreate($userProfile);
             Auth::login($user, true);
 
             return redirect()->intended('/');
         } catch (\Exception $e) {
-
             return redirect('/login')->with('error', 'Hubo un problema al iniciar sesión con Azure.');
         }
     }
+
 
     public function findOrcreate($userProfile)
     {
@@ -59,7 +93,6 @@ class AzureController extends Controller
         $user = User::where('email', $mail)->orWhere('email', $userPrincipalName)->first();
 
         if (!$user) {
-            // Crear un nuevo usuario si no existe
             $name = $userProfile['displayName'] ?? $undefined;
             $company = $userProfile['companyName'] ?? $undefined;
             $country = $userProfile['country'] ?? $undefined;
@@ -100,7 +133,7 @@ class AzureController extends Controller
                     break;
 
                 default:
-                
+
                     break;
             }
 
@@ -112,7 +145,7 @@ class AzureController extends Controller
                     $name = $workspaceName[1];
                     $type = 'user';
                 } else {
-                    $arrayDepartment = explode('/', $department); 
+                    $arrayDepartment = explode('/', $department);
                     $workspaceName = explode(' ', $arrayDepartment[0]);
                     $name = $workspaceName[1];
                     $type = in_array($arrayDepartment[1], ['Técnico', 'Sistemas', 'I+D']) ? 'user' : 'client';
@@ -128,18 +161,19 @@ class AzureController extends Controller
 
             // Si el usuario no existe, lo creamos
             $user = User::create([
-                'name' => $name, 
+                'name' => $name,
                 'userPrincipalName' => $userPrincipalName,
                 'email' => $mail,
                 'company' => $company,
-                'branch' => $branch, 
-                'department' => $department, 
+                'branch' => $branch,
+                'department' => $department,
                 'country' => $country,
                 'jobTitle' => $jobTitle,
                 'officeLocation' => $location,
                 'type' => $type,
-                'currant_workspace' => $workspace->id,
+                'currant_workspace' => $workspace ? $workspace->id : '',
                 'email_verified_at' => now(),
+                'avatar' => $userProfile['photo_path'] ?? null,
             ]);
 
             UserWorkspace::create([
