3d82c10bc51525d8983d1c15f46f5bab2d55bdfc
alsitectools <alsitectools@alsina.com>
2026-07-21 11:27:51 +0200

    Fix: update avatar image source to use asset helper for correct path resolution

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1620,7 +1620,7 @@
                                                         @if ($milestone->getRequestedBy() != null)
                                                             <img class="fix_img"
                                                                 title="{{ $milestone->getRequestedBy()->name }}"
-                                                                 @if ($milestone->getRequestedBy()->avatar) src="{{ $milestone->getRequestedBy()->avatar }}" @else avatar="{{ $milestone->getRequestedBy()->name }}" @endif>
+                                                                 @if ($milestone->getRequestedBy()->avatar) src="{{ asset($milestone->getRequestedBy()->avatar) }}" @else avatar="{{ $milestone->getRequestedBy()->name }}" @endif>
                                                         @endif
                                                     </div>
                                                     <div class="project-order-td" data-col-key="assigned_to"
@@ -1628,7 +1628,7 @@
                                                         @if ($milestone->getAssignedToUser() != null)
                                                             <img class="fix_img"
                                                                 title="{{ $milestone->getAssignedToUser()->name }}"
-                                                                 @if ($milestone->getAssignedToUser()->avatar) src="{{ $milestone->getAssignedToUser()->avatar }}" @else avatar="{{ $milestone->getAssignedToUser()->name }}" @endif>
+                                                                 @if ($milestone->getAssignedToUser()->avatar) src="{{ asset($milestone->getAssignedToUser()->avatar) }}" @else avatar="{{ $milestone->getAssignedToUser()->name }}" @endif>
                                                         @else
                                                             ...
                                                         @endif
